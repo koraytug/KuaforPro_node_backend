683a5734677ac66587719bbd69fbf6ffcfbe725d
Koray Tug <koray.tug@virtudev.net>
2022-02-06 15:28:57 +0300

    add MVC architecture

---
+++ src/routes/customer.route.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+import CustomerController from "../controllers/customer.controller";
+import {ICustomerController} from "../controllers/icustomer.controller";
+
+export default class AppRouter {
+    public appRouter = express.Router();
+    public app: ICustomerController = new CustomerController();
+
+    constructor() {
+        this.appRouter.get("/getall_data", this.app.getAllCustomers); 
+    }
+
+}
+
